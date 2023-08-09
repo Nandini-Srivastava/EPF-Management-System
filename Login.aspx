ba4d13718932e1fee1f8d49523aaf5832c8d83a6
Nandini-Srivastava <90909778+Nandini-Srivastava@users.noreply.github.com>
2023-08-09 10:18:19 +0530

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="lblErr" runat="server" ForeColor="Red"></asp:Label><br />
+            user:  
+            <asp:TextBox runat="server" ID="txtUser"></asp:TextBox>
+            Pass:
+            <asp:TextBox runat="server" ID="txtPass"></asp:TextBox>
+            <asp:Button runat="server" ID="btnpass" OnClick="btnpass_Click" Text="Submit" />
+            <asp:Button runat="server" ID="btnReg" OnClick="btnReg_Click" Text="Register" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnpass_Click(object sender, EventArgs e)
+    {
+        OracleConnection myConn = new OracleConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DSNTrainee"].ToString());
+        OracleCommand cmd = new OracleCommand();
+        OracleDataReader dr;
+        string Sql = "";
+        string dt = "";
+
+        try
+        {
+            Sql = "SELECT USERNAME, PASSWORD FROM VT_LOGIN ";
+            cmd.CommandText = Sql;
+            cmd.Connection = myConn;
+            if (myConn.State == System.Data.ConnectionState.Closed)
+                myConn.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+
+                if (txtUser.Text.Trim() == dr.GetValue(0).ToString() && txtPass.Text.Trim() == dr.GetValue(1).ToString())
+                {
+                    Response.Redirect("About.aspx");
+                }
+                else
+                {
+                    lblErr.Text = "Error in login.Wrong user id and password";
+
+                }
+
+            }
+            else
+            {
+                lblErr.Text = "No record";
+            }
+        }
+        catch (Exception ee)
+        {
+            lblErr.Text = "Error in login.";
+        }
+        finally
+        {
+            if (myConn.State == System.Data.ConnectionState.Open)
+                myConn.Close();
+        }
+    }
+    protected void btnReg_Click(object sender, EventArgs e)
+    {
+        OracleConnection myConn = new OracleConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DSNTrainee"].ToString());
+        OracleCommand cmd = new OracleCommand();
+        OracleDataReader dr;
+        string Sql = "";
+        string dt = "";
+
+        try
+        {
+            Sql = "insert into  VT_LOGIN  (USERNAME, PASSWORD) values ('" + txtUser.Text.Trim() + "','" + txtPass.Text.Trim() + "')";
+            cmd.CommandText = Sql;
+            cmd.Connection = myConn;
+            if (myConn.State == System.Data.ConnectionState.Closed)
+                myConn.Open();
+            int rows=  cmd.ExecuteNonQuery();
+
+            if (rows > 0) {
+                lblErr.Text = "record inserted";
+            }
+           
+        }
+        catch (Exception ee)
+        {
+            lblErr.Text = "Error in login.";
+        }
+        finally
+        {
+            if (myConn.State == System.Data.ConnectionState.Open)
+                myConn.Close();
+        }
+    }
+}
